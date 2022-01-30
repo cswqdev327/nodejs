f50b24ca7b41f91517d4b01beaa693508cb73bee
cswqdev327 <cswqdev327@gmail.com>
2022-01-30 18:47:51 +0900

    add simple routes

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+
+module.exports.index = index;
+module.exports.login = login;
+module.exports.loginProcess = loginProcess;
+module.exports.chat = chat;
+
+function index( req, res ) {
+    res.send('index');
+}
+
+function login( req, res ) {
+    res.send('login');
+}
+
+function loginProcess( req, res ) {
+    res.redirect('/');
+}
+
+function chat( req, res ) {
+    res.send('chat');
+}
